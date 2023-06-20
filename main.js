7371c2c5ec0ff81c296cb899ada54acc8ccc5174
Tommy <samsanite25@gmail.com>
2023-06-19 21:07:02 -0700

    add ender

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 console.log('connected')
-
+// get search field input
 const searchTermInput = documen.body.querySelector("#search-term");
 
 // fetching API data
+
 const getMealCateregories= async () => {  //async to avoid jumping og code
     const mealCateogisApiURL = "GET https://www.mmobomb.com/api1/games?sort-by=alphabetical"
+    
     try {
         const response = await fetch(mealCateogisApiURL)
         const data = await response.json()
@@ -14,12 +16,21 @@ const getMealCateregories= async () => {  //async to avoid jumping og code
         console.log(error)
         alert('Something went wrong, try again later')
     }
-}
-
-const handleFormInputFocus = async () => {   //new fetch
-    console.log('focus occurred');
-
-    await getMealCateregories();
 };
 
+//render meal categories data to DOM
+const renderMealCategories = (mealCategoriesOb) => {
+    console.log('renderMealCategories');
+    console.table(mealCategoriesOb);
+};
+
+//function to call form input is given focus
+const handleFormInputFocus = async () => {   //new fetch
+    console.log('focus occurred');
+    
+    const mealCategoriesOb = await getMealCateregories();
+    renderMealCategories(mealCategoriesOb);
+};
+
+//add eventlistner to search term input
 searchTermInput.addEventListener('focus', handleFormInputFocus);
